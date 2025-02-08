2f74ef2a35e3bce24a90a1796336122124a6654c
nabnab00 <nabilkhan2010@live.com>
2025-02-08 16:57:52 -0500

    full upload code
    FileUploader.js functional code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,5 @@
-import './App.css';
-import logo from './logo.svg';
 import FileUploader from './components/FileUploader';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          test123
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-  
   return (
     <div className="App">
       <FileUploader />
