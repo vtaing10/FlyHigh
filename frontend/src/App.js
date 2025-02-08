877c2b8a18d2adefb41a087ca81228a57add04c4
nabnab00 <nabilkhan2010@live.com>
2025-02-08 15:06:50 -0500

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          12334567
+          test123
         </p>
         <a
           className="App-link"
