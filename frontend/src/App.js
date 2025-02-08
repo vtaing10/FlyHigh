60ea3c99764e0d5320ac638526e1594467013508
NithushanKana <nithushan.kana@gmail.com>
2025-02-08 14:26:58 -0500

    nithu tesat

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          test123
+          12334567
         </p>
         <a
           className="App-link"
